8b28178360164a0cb161538502125b7a1d283590
Okrag <okragtv@gmail.com>
2020-12-10 11:21:27 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const copyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    testportal: "./src/testportal/index.ts",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "[name].js",
+  },
+  plugins: [
+    new copyPlugin({
+      patterns: [{ from: "./public", to: "../" }],
+    }),
+  ],
+};
